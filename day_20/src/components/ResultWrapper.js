faf1c2edb114593c9f8762ae20bdf3ea342c1b93
dhanush_arun03 <61613602+dhanusharun@users.noreply.github.com>
2023-07-04 17:42:48 +0530

    Day 20 - Project - Anime finder -Day 1

---
+++ day_20/src/components/ResultWrapper.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ResultWrapper = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ResultWrapper
---
+++ day_20/src/pages/ResultPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import SearchBar from '../components/SearchBar';
+
+const ResultPage = () => {
+  return (
+    <div className='main-wrapper'>
+    <div className='row'>
+      <h1>Result Page</h1>
+    </div>
+    <div className='row'>
+      <SearchBar />
+    </div>
+    </div>
+  )
+}
+
+export default ResultPage
